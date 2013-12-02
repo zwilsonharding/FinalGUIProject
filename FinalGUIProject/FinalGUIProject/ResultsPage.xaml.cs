1a3e40fed029141d50ebf8f2cf7f59a1543011fe
zwilsonharding <zwilson@harding.edu>
2013-12-01 20:31:12 -0600

    added Numbers API

--- FinalGUIProject/FinalGUIProject/ResultsPage.xaml.cs
+++ FinalGUIProject/FinalGUIProject/ResultsPage.xaml.cs
@@ -11,6 +11,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Net;
+using System.Threading;
+using System.Text;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -18,9 +21,13 @@ namespace FinalGUIProject {
 	/// <summary>
 	/// An empty page that can be used on its own or navigated to within a Frame.
 	/// </summary>
-	public sealed partial class ResultsPage : Page {
-		public ResultsPage() {
-			this.InitializeComponent();
+	public sealed partial class ResultsPage : Page 
+    {
+		public ResultsPage() 
+        {
+            this.InitializeComponent();
+            APIGetResponseDate(((Application.Current as App).selectedMonth + 1).ToString() + "/" + ((Application.Current as App).selectedDay + 1).ToString());
+            APIGetResponseYear(((Application.Current as App).selectedYear + 1950).ToString());
 		}
 
 		/// <summary>
@@ -40,5 +47,30 @@ namespace FinalGUIProject {
 				this.Frame.Navigate(typeof(MainPage));
 			}
 		}
+
+        private async void APIGetResponseDate(string date)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://numbersapi.com/" + date + "/date");
+
+            HttpWebResponse res = (HttpWebResponse)(await req.GetResponseAsync());
+            Stream streamResponse = res.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResponse);
+            string responseString = await streamRead.ReadToEndAsync();
+
+            APIDate.Text = responseString;
+        }
+
+        private async void APIGetResponseYear(string year)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://numbersapi.com/" + year + "/year");
+
+            HttpWebResponse res = (HttpWebResponse)(await req.GetResponseAsync());
+            Stream streamResponse = res.GetResponseStream();
+            StreamReader streamRead = new StreamReader(streamResponse);
+            string responseString = await streamRead.ReadToEndAsync();
+
+            APIYear.Text = responseString;
+        }
+
 	}
 }
